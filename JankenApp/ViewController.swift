2bed2aea6de43027dd2617eecc8dda050f6f24ae
優樹永井 <wadachi0505@gmail.com>
2019-02-25 15:15:44 +0900

    first commit

--- JankenApp/ViewController.swift
+++ JankenApp/ViewController.swift
@@ -9,10 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //IB変数宣言
+    @IBOutlet var jankenImageView: UIImageView!
+    @IBOutlet var jankenLabel: UILabel!
+    
+    //変数宣言
+    var answerNumber = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func shuffleAction() {
+        
+        //新しいじゃんけんの結果を一時的に格納する変数を設ける
+        var newAnswerNumber = 0
+        
+        repeat {
+            newAnswerNumber = Int(arc4random_uniform(3))
+            
+            //前回と同じ結果の時は再度ランダムに数値を出す
+            //異なる結果の時はrepeatを抜ける
+        } while answerNumber == newAnswerNumber
+        
+        //新しいじゃんけんの結果を格納
+        answerNumber = newAnswerNumber
+        
+        if answerNumber == 0 {
+            //グー
+            jankenLabel.text = "グー"
+            jankenImageView.image = UIImage(named: "gu.jpg")
+        } else if answerNumber == 1 {
+            //チョキ
+            jankenLabel.text = "チョキ"
+            jankenImageView.image = UIImage(named: "tyoki.jpg")
+        } else if answerNumber == 2 {
+            //パー
+            jankenLabel.text = "パー"
+            jankenImageView.image = UIImage(named: "pa.png")
+        }
+        
     }
 
 
